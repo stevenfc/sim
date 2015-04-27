eabd3dcf4c1cd3469c358aa384472322ab4b21fb
stevenfc <stevenfc1120@gmail.com>
2015-04-27 13:14:49 -0600

    cambiando el nombre del paquete

--- SIM/src/java/com/SIM/common/domain/Compania.java
+++ SIM/src/java/com/SIM/common/domain/Compania.java
@@ -1,10 +1,9 @@
-package com.SIM.common.domain;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package com.sim.common.domain;
 
 /**
  *
